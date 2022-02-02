68d6309b7dd2364323c9b4e83988807a03d8dcdc
Fabiojpl1907 <fabiojpl.1907@gmail.com>
2022-02-02 11:32:08 -0300

    Refazer propjeto Angular

--- 4_VagasAngular/src/app/models/Vagas.models.ts
+++
@@ -1,8 +0,0 @@
-class Vaga{
-    id: number = 0;
-    descricao: string = "";
-    req_obrigatorio:  string = "";
-    req_desejaveis:  string = "";
-    remuneracao: number= 0;
-    local_trabalho:  string = "";
-}
