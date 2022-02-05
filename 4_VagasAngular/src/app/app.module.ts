fab7d0d45f03607c4133852c42496cd510eed8ea
Fabiojpl1907 <fabiojpl.1907@gmail.com>
2022-02-05 10:28:03 -0300

    Criado componente Mural Vagas

---
+++ 4_VagasAngular/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MuralVagasComponent } from './mural-vagas/mural-vagas.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MuralVagasComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
